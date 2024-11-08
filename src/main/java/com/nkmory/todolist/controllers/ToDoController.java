cab27b6acc191d1e3ed9cb8b7294a8b5033bfa0b
Nika Kurafeeva <nikurafeeva@gmail.com>
2024-11-08 12:40:13 +0300

    fixed adding tasks, doesn't set them to null anymore
    -- only shows added tasks after the page is reloaded
    -- after reloading adding tasks returns errors, but still adds them to the list and shows them after the next reload
    -- when trying to load tasks via "Load" button throughs an error for trying to read undefined. unloads all the tasks already on screen(they remain in the list and get loaded on the next page reload)

--- src/main/java/com/nkmory/todolist/controllers/ToDoController.java
+++ src/main/java/com/nkmory/todolist/controllers/ToDoController.java
@@ -1,11 +1,17 @@
+//Even though it shows an error do NOT delete it
 package com.nkmory.todolist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.nkmory.todolist.models.ToDo;
 import com.nkmory.todolist.services.ToDoService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/todos")
